e6bd8f091d43bc5120f188ad6ae4afda9ae42733
Iftikhar <miftikhar@digitalgravity.ae>
2024-10-25 21:14:15 +0500

    up!

--- app/(admin)/settings/inventory/locations/page.tsx
+++ app/(admin)/settings/inventory/locations/page.tsx
@@ -30,12 +30,12 @@ import { convertToDateTime } from "@/app/Utility/helperFunctions";
 import Feedback from "@/app/components/Feedback";
 import Alert from "@/app/components/Alert";
 import { ImageWithTextComp } from "@/app/components/Dashboard/DataGrid/DataGridComponents";
-// import "ag-grid-enterprise";
-// import { LicenseManager } from "ag-grid-enterprise";
+import "ag-grid-enterprise";
+import { LicenseManager } from "ag-grid-enterprise";
 
-// LicenseManager.setLicenseKey(
-//   "Using_this_{AG_Grid}_Enterprise_key_{AG-069569}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Digital_Gravity_Solution_LLC}_is_granted_a_{Multiple_Applications}_Developer_License_for_{1}_Front-End_JavaScript_developer___All_Front-End_JavaScript_developers_need_to_be_licensed_in_addition_to_the_ones_working_with_{AG_Grid}_Enterprise___This_key_has_not_been_granted_a_Deployment_License_Add-on___This_key_works_with_{AG_Grid}_Enterprise_versions_released_before_{15_November_2025}____[v3]_[01]_MTc2MzE2NDgwMDAwMA==7f6c7223ab4682288a6572584247da48"
-// );
+LicenseManager.setLicenseKey(
+  "Using_this_{AG_Grid}_Enterprise_key_{AG-069569}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Digital_Gravity_Solution_LLC}_is_granted_a_{Multiple_Applications}_Developer_License_for_{1}_Front-End_JavaScript_developer___All_Front-End_JavaScript_developers_need_to_be_licensed_in_addition_to_the_ones_working_with_{AG_Grid}_Enterprise___This_key_has_not_been_granted_a_Deployment_License_Add-on___This_key_works_with_{AG_Grid}_Enterprise_versions_released_before_{15_November_2025}____[v3]_[01]_MTc2MzE2NDgwMDAwMA==7f6c7223ab4682288a6572584247da48"
+);
 
 const LocationsPage = () => {
   const [addLocationDrawer, setAddLocationDrawer] = useState(false);
--- app/components/Dashboard/DataGrid/index.tsx
+++ app/components/Dashboard/DataGrid/index.tsx
@@ -34,6 +34,7 @@ import {
 import { AG_GRID_PAGE_SIZE } from "@/app/Utility/constants";
 import Spinner from "../../Spinner";
 import NoRecordFound from "../../NoRecordFound";
+import { LicenseManager } from "ag-grid-enterprise";
 
 export interface DataGridProps {
   columnData: any;
@@ -54,6 +55,10 @@ export interface DataGridProps {
   key?: any;
   quickFilerText?: any;
 }
+
+LicenseManager.setLicenseKey(
+  "Using_this_{AG_Grid}_Enterprise_key_{AG-069569}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Digital_Gravity_Solution_LLC}_is_granted_a_{Multiple_Applications}_Developer_License_for_{1}_Front-End_JavaScript_developer___All_Front-End_JavaScript_developers_need_to_be_licensed_in_addition_to_the_ones_working_with_{AG_Grid}_Enterprise___This_key_has_not_been_granted_a_Deployment_License_Add-on___This_key_works_with_{AG_Grid}_Enterprise_versions_released_before_{15_November_2025}____[v3]_[01]_MTc2MzE2NDgwMDAwMA==7f6c7223ab4682288a6572584247da48"
+);
 
 const getServerSideDatasource: (server: any) => IServerSideDatasource = (
   server: any
